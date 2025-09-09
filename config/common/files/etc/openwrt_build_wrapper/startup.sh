b69d991cdc9dee0881ba29775e0692f91ae5bc92
dezeroku <contact@dezeroku.com>
2025-09-09 23:56:39 +0200

    config: common: add custom_commands_persistent
    This value allows defining commands that should be run on every reboot

--- config/common/files/etc/openwrt_build_wrapper/startup.sh
+++ config/common/files/etc/openwrt_build_wrapper/startup.sh
@@ -18,26 +18,28 @@ echo "Starting acme"
 # shellcheck disable=SC2017,SC2016
 if [ ! -f '/etc/ssl/acme/{{ .hostname }}.{{ .letsencrypt.domain }}.crt' ]; then
 	echo "Couldn't obtain cert, STOP"
-	exit 1
 else
+	echo "DONE"
+
+	echo "Align uhttpd to use ACME obtained certs"
+
+	# shellcheck disable=SC2017,SC2016
+	uci set uhttpd.main.cert='/etc/ssl/acme/{{ .hostname }}.{{ .letsencrypt.domain }}.crt'
+	# shellcheck disable=SC2017,SC2016
+	uci set uhttpd.main.key='/etc/ssl/acme/{{ .hostname }}.{{ .letsencrypt.domain }}.key'
+
+	# Make sure default cert generation will not mess up the symlinks
+	uci -q get uhttpd.defaults && uci delete uhttpd.defaults
+
+	uci commit uhttpd
+	echo "DONE"
+
+	echo "Restarting uhttpd"
+	/etc/init.d/uhttpd restart
 	echo "DONE"
 fi
 
-echo "Align uhttpd to use ACME obtained certs"
-
-# shellcheck disable=SC2017,SC2016
-uci set uhttpd.main.cert='/etc/ssl/acme/{{ .hostname }}.{{ .letsencrypt.domain }}.crt'
-# shellcheck disable=SC2017,SC2016
-uci set uhttpd.main.key='/etc/ssl/acme/{{ .hostname }}.{{ .letsencrypt.domain }}.key'
-
-# Make sure default cert generation will not mess up the symlinks
-uci -q get uhttpd.defaults && uci delete uhttpd.defaults
-
-uci commit uhttpd
-echo "DONE"
-
-echo "Restarting uhttpd"
-/etc/init.d/uhttpd restart
-echo "DONE"
 # shellcheck disable=SC1009,SC1054,SC1056,SC1072,SC1073,SC1083
 {{- end }}
+
+{{ .custom_commands_persistent }}
